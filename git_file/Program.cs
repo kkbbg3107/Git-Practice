c965a9328d3bcee4e4568b3aacf7e2faeed50ebe
KAIHSUN KUO <kkbbg3107@gmail.com>
2022-04-10 19:39:16 +0800

    Practice git

---
+++ git_file/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace git_file
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
